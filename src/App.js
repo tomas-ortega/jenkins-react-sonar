a50a4acd94d474e1127e9a20808d9d764b3de561
Manuel Tomás Ortega Sánchez <m.tomas.ortega@gmail.com>
2020-04-21 09:31:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
-import
-import
-import
-import
-import
-import
-React from 'react';
-{ Route, Switch, Redirect } from 'react-router-dom';
-ListaOfertas from './components/ListaOfertas';
-Form from './components/Form';
-Nav from './components/Nav';
-OfertaDetallada from './components/OfertaDetallada';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import ListaOfertas from './components/ListaOfertas';
+import Form from './components/Form';
+import Nav from './components/Nav';
+import OfertaDetallada from './components/OfertaDetallada';
+
 class App extends React.Component {
-  render() {
- 
+render() {
 return (
- 
 <div>
- 
 <Nav />
- 
 <Switch>
- 
 <Route path="/ofertas/:id" component={OfertaDetallada} />
- 
 <Route path="/editar-oferta/:id" component={Form} />
- 
 <Route path="/ofertas" component={ListaOfertas} />
- 
 <Route path="/nueva-oferta" component={Form} />
- 
 <Redirect from="/" exact to="/ofertas" />
- 
 </Switch>
- 
 </div>
- 
 );
-  }
+}
 }
 export default App;
